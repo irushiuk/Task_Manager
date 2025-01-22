886b387df2b598f262c39fd7f6d7149066a45387
irushiuk <irushiudyani@gmail.com>
2025-01-22 06:04:27 +0530

    authentication(error)

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,36 +1,33 @@
-import bcrypts from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import mongoose, { Schema } from 'mongoose';
 
-
-const userSchema = new Schema (
-    {
-    name :{type:String,required: true},
-    email: { type:String, requied:true, unique:true},
-    password:{type:String,required: true},
-    isAdmin :{type:Boolean,required: true, default:false},
-    tasks :{type:[String],required: true},
-    isActive: {type:Boolean, requied:true, default:true},
-    },
-
-    {timestamps:true}
-
-
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    tasks: { type: [String], required: true, default: [] },
+    isActive: { type: Boolean, required: true, default: true },
+  },
+  { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+// Pre-save middleware to hash passwords
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-userSchema.method.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-    
-}
+// Method to match passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model('User', userSchema);
 
 export default User;
