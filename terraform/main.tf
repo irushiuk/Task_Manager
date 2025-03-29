c0077a7606c2bb992c4352fc95a1b494f0fbfae2
irushiuk <irushiudyani@gmail.com>
2025-03-29 18:00:35 +0530

    ami updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04
+  ami           = "ami-08c40ec9ead489470" # Ubuntu 20.04
   instance_type = "t2.micro"
   key_name      = "my-key-pair"
 
